a115dc37d383aa1dbaacce372a58f4d9eab9a506
alcole <alexjcole01@gmail.com>
2015-01-16 21:16:04 +0000

    added mean and variance

--- GamblersRuin/GamblersRuin.m
+++ GamblersRuin/GamblersRuin.m
@@ -1,11 +1,13 @@
-function [ ] = GamblersRuin( a, j, p )
+function [ ] = GamblersRuin( a, j, p, n )
 %GAMBLERSRUIN Summary 
 % Gamblers ruin in the style of a simple random walk, where p + q = 1
 %  a - The Gambler and opponent have £a between them
 %      If the opponent has unlimited resources set a to Inf
 %  j - The Gambler begins with £j
 %  p - The probability the Gambler wins a round of the game
-%  q - The probability the Gambler looses a round of the game = 1 - p
+%  n - (Optional) the number of steps 
+%  q - (caluclated)The probability the Gambler looses a round of the game = 1 - p
+q = 1 - p;
 
 % Probability the Gambler is ruined
 qj = GamblersRuinProb( a, j, p); 
@@ -13,13 +15,20 @@ qj = GamblersRuinProb( a, j, p);
 % Expected duration of the game
 Dj = GamblersRuinDuration( a, j, p);
 
-
+% print summary
 fprintf('The probability the Gambler looses is %f\n', qj);
 if Dj < 0
     fprintf('There is no expected duration of the game\n');
 else
     fprintf('The expected duration of the game is %.2f rounds\n ', Dj);
 end 
+fprintf('\n');
+
+%calculate mean and variance if number of steps supplied
+if nargin == 4
+    fprintf('E(X_n) = %.4f\n', n*(p-q));
+    fprintf('V(X_n) = %.4f\n', 4*n*p*q);
+end
 
 end
 
