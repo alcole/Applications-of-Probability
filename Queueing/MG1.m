4ec5826131ddae9da98918cb6ff0d3e06097515f
alcole <alexjcole01@gmail.com>
2015-02-26 21:36:51 +0000

    Added M/G/1 queue mean equilibrium

---
+++ Queueing/MG1.m
@@ -0,0 +1,20 @@
+function [ rho ] = MG1( lambda, mean, variance )
+%MG1 calculates the traffic intensity and mean equilibrium queue size
+%   Detailed explanation goes here
+
+% traffic intensity rho
+rho = lambda * mean;
+
+if rho < 1
+    % equilibrium queue size exists
+    % calculate mean with Pollaczek's formula
+    EX = (rho - 0.5 * rho^2 + 0.5 * lambda^2 * variance) / (1 - rho);
+    
+    fprintf('Mean equilibrium queue size = %.4f\n', EX);
+else
+    fprintf('Equilibrium queue size distribution does not exist\n');
+end
+
+
+end
+
