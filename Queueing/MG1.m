54872162bd2e07ef0949cadbfe603919d8b70e49
alcole <alexjcole01@gmail.com>
2015-03-03 19:43:47 +0000

    Changed return value to mean

--- Queueing/MG1.m
+++ Queueing/MG1.m
@@ -1,4 +1,4 @@
-function [ rho ] = MG1( lambda, mean, variance )
+function [ EX ] = MG1( lambda, mean, variance )
 %MG1 calculates the traffic intensity and mean equilibrium queue size
 %   for the M/G/1 model of queues
 
@@ -11,6 +11,7 @@ if rho < 1
     EX = (rho - 0.5 * rho^2 + 0.5 * lambda^2 * variance) / (1 - rho);
     
     fprintf('Mean equilibrium queue size = %.4f\n', EX);
+    fprintf('Rho = %.4f\n', rho);
 else
     fprintf('Equilibrium queue size distribution does not exist\n');
 end
