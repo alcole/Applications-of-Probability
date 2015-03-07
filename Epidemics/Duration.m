3d100fe9db3feedce1f7348dad51242e9ceb60a8
alcole <alexjcole01@gmail.com>
2015-03-07 22:00:58 +0000

    add variance for the duration

--- Epidemics/Duration.m
+++ Epidemics/Duration.m
@@ -1,13 +1,16 @@
-function [ W ] = Duration( Beta, y0, n )
+function [ W, VW ] = Duration( Beta, y0, n )
 %DURATION Caluclates the duration of an epidemic
 %   W = the waiting time until all the susceptibles are infected
 %   W = T_y0 + T_y0+1 + \ldots + T_n
+%   WV = Variance of the expected waiting time
 
 W = 0;
+VW = 0;
 
 for i=y0:n-1
-    wn = ( 1/ EpidemicRate(Beta, i, n-1));
-    W = W + wn;
+    [E, V] = ( WaitingTimesParams(EpidemicRate(Beta, i, n-1)));
+    W = W + E;
+    VW = VW + V;
     i = i + 1;
 end
 
