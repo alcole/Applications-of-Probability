4f5dc1a564c96fc0fade01d0b759d9c77bacef9b
alcole <alexjcole01@gmail.com>
2015-03-09 22:06:19 +0000

    fixed n-correction

--- Epidemics/EpidemicRate.m
+++ Epidemics/EpidemicRate.m
@@ -2,8 +2,10 @@ function [ Beta_y ] = EpidemicRate( Beta, y, n )
 %EPIDEMICRATE Calculates the epidemic rate Beta_y
 %   Beta    = rate of the Poisson process
 %   y       = number of individuals of a certain type
-%   n       = community of size n + 1
+%   n       = community size
 %   x       = no of individuals of the other type
+
+n = n - 1;
 
 x = n + 1 - y;
 Beta_y = (Beta * y * x) / n;
